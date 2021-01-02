033def2daf122d9950782f67e0163bc8a3df964c
Nicolai Buck <nicobuck93@gmail.com>
2021-01-02 12:02:57 +0100

    WIP

--- keyboards/crkbd/keymaps/n-buck/config.h
+++ keyboards/crkbd/keymaps/n-buck/config.h
@@ -18,6 +18,9 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
+#define PERMISSIVE_HOLD
+#define IGNORE_MOD_TAP_INTERRUPT
+#define RETRO_TAPPING
 #pragma once
 
 //#define USE_MATRIX_I2C
--- keyboards/crkbd/keymaps/n-buck/keymap.c
+++ keyboards/crkbd/keymaps/n-buck/keymap.c
@@ -49,18 +49,14 @@ enum macro_keycodes {
 };
 
 
-const uint16_t mod_esc = MT(MOD_LALT | MOD_LGUI, KC_ESC);
-const uint16_t mod_lc  = MT(MOD_LCTL, KC_Z);
-const uint16_t mod_rc  = MT(MOD_LCTL, KC_SLASH);
-
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_QWERTY] = LAYOUT_split_3x6_3( \
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-      mod_esc,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                         KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,  KC_BSPC,\
+LCAG_T(KC_ESC),   KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                         KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,  KC_BSPC,\
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       KC_LGUI,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                         KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, KC_QUOT,\
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_LSFT,  mod_lc,    KC_X,    KC_C,    KC_V,    KC_B,                         KC_N,    KC_M, KC_COMM,  KC_DOT,  mod_rc, KC_RSFT,\
+    KC_LSFT  ,LCTL_T(KC_Z),KC_X,    KC_C,    KC_V,    KC_B,                         KC_N,    KC_M, KC_COMM,  KC_DOT,  RCTL_T(KC_SLASH), KC_RSFT,\
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
                                           KC_LALT,   LOWER,  KC_SPC,     KC_ENT,  RAISE,  KC_RALT \
                                       //`--------------------------'  `--------------------------'
@@ -69,13 +65,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_LOWER] = LAYOUT_split_3x6_3( \
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-       KC_ESC,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                         KC_7,    KC_8,    KC_9,    KC_0, KC_BSPC, KC_6,\
+    KC_ESC,   KC_F1,   KC_F2,   KC_F3,   KC_F4,  KC_PGUP,                      KC_7,    KC_8,    KC_9,  KC_LBRACKET, KC_RBRACKET, KC_DEL,\
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_LCTL, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                         KC_4,    KC_5,    KC_6,KC_RIGHT, XXXXXXX, XXXXXXX,\
+    KC_LCTL,  KC_F5,   KC_F6,   KC_F7,   KC_F8,  KC_PGDOWN,                    KC_4,    KC_5,    KC_6,    KC_EQUAL, KC_MINUS, KC_BSLASH,\
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_LSFT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                         KC_1,    KC_2,    KC_3, XXXXXXX, XXXXXXX, XXXXXXX,\
+    KC_LSFT,  KC_F9,   KC_F10,  KC_F11,  KC_F12, KC_INSERT,                    KC_1,    KC_2,    KC_3,    KC_TILDE, KC_PSCREEN, KC_SLASH,\
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                          KC_LGUI,   LOWER,  KC_SPC,     KC_ENT,   RAISE, KC_TAB \
+                                          KC_LGUI,   LOWER,  KC_SPC,     KC_TAB,   KC_0, KC_DOT \
                                       //`--------------------------'  `--------------------------'
     ),
 
@@ -87,7 +83,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       KC_LSFT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      KC_UNDS, KC_PLUS, KC_LBRC, KC_RBRC, KC_BSLS, KC_TILD,\
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                          KC_LGUI,   LOWER,  KC_SPC,     KC_ENT,   RAISE, KC_RALT \
+                                          KC_LGUI,   LOWER,  KC_BSPC,    KC_ENT,   RAISE, KC_RALT \
                                       //`--------------------------'  `--------------------------'
   ),
 
